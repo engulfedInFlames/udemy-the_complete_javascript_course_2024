446940d9e060ecbbcc2204b9a6f4cc5678755b57
engulfedinflames <recona97@gmail.com>
2024-03-23 17:28:19 +0900

    display user icon when it's one's own recipe

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -81,7 +81,7 @@ class RecipeView extends View {
         </div>
       </div>
     
-      <div class="recipe__user-generated">
+      <div class="recipe__user-generated ${this._data.key ? "" : "hidden"}">
         <svg>
           <use href="${iconsUrl}#icon-user"></use>
         </svg>
