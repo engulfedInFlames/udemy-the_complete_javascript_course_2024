d85238ecad60aec0233f0e82c70de110465e6954
engulfedinflames <recona97@gmail.com>
2024-03-23 17:25:48 +0900

    add api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const API_KEY = "acdd6ef0-4ffd-42ff-83ec-4e384de6f5df";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
